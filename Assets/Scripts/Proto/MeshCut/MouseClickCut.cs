707abc2f6e56d034c4ddb908b5d91105aa9e179d
Viktor Chernikov <vktrchernikov@gmail.com>
2024-10-23 18:18:54 +0200

    feat: Updated project version, imported first person controller, imported slashing system

---
+++ Assets/Scripts/Proto/MeshCut/MouseClickCut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickCut : MonoBehaviour
+{
+    void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+
+			
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 4f)){
+				GameObject victim = hit.collider.gameObject;
+				if(victim.tag == "Cuttable")
+                {
+					Debug.Log("Cut");
+                    Cutter.Cut(victim, hit.point, Camera.main.transform.right);
+                }
+			}
+
+		}
+	}
+}
