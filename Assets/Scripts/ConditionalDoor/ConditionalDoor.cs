642fe83beca1e824d7ce97370117c4ebc631e08d
Viktor Chernikov <vktrchernikov@gmail.com>
2024-11-21 00:32:46 +0100

    feat: Migrated evevator visuals and scripts from my older project for conditional door purposes

--- Assets/Scripts/ConditionalDoor/ConditionalDoor.cs
+++ Assets/Scripts/ConditionalDoor/ConditionalDoor.cs
@@ -1,31 +1,97 @@
+using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class ConditionalDoor : MonoBehaviour
 {
-    [SerializeReference] private List<PuzzleCondition> _activators = new List<PuzzleCondition>();
-    private Transform _transform;
+    [Header("State")]
+    public ConditionalDoorState state = ConditionalDoorState.Closed;
+
+    [Header("Sounds")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] openSounds;
+    [SerializeField] AudioClip[] closeSounds;
+
+    [Header("Parameters")]
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] Vector3 closedLocalPos = Vector3.zero;
+    [SerializeField] Vector3 openedLocalPos = Vector3.zero;
+
+
+    [SerializeReference] private List<PuzzleCondition> activators = new List<PuzzleCondition>();
     
     private void Awake()
     {
-        _activators.ForEach(doorActivator => doorActivator.onFulfilmentChange.AddListener(ConditionHandler));
-        _transform = GetComponent<Transform>();
+        activators.ForEach(doorActivator => doorActivator.onFulfilmentChange.AddListener(ConditionHandler));
     }
 
     private void ConditionHandler()
     {
-        if (_activators.All(activator => activator.GetConditionFulfilment())) Open();
+        if (activators.All(activator => activator.GetConditionFulfilment())) Open();
         else Close();
     }
 
-    private void Open()
+    public void Open()
     {
-        _transform.rotation = Quaternion.Euler( _transform.rotation.eulerAngles.x, 90,  _transform.rotation.eulerAngles.z);
+        StartCoroutine(OpenC());
+    }
+    public void Close()
+    {
+        StartCoroutine(CloseC());
     }
 
-    private void Close()
+    public IEnumerator OpenC()
     {
-        _transform.rotation = Quaternion.Euler( _transform.rotation.eulerAngles.x, 0,  _transform.rotation.eulerAngles.z);
+        if (state == ConditionalDoorState.Closed)
+        {
+            PlayRandom(openSounds);
+            yield return MoveGate(true);
+        }
     }
+    public IEnumerator CloseC()
+    {
+        if (state == ConditionalDoorState.Opened)
+        {
+            PlayRandom(closeSounds);
+            yield return MoveGate(false);
+        }
+    }
+
+    IEnumerator MoveGate(bool opened)
+    {
+        state = ConditionalDoorState.Busy;
+        Vector3 destination = opened ? openedLocalPos : closedLocalPos;
+
+        while (Vector3.Distance(transform.localPosition, destination) > 0.05f)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, destination, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.localPosition = destination;
+        state = opened ? ConditionalDoorState.Opened : ConditionalDoorState.Closed;
+    }
+    void PlayRandom(AudioClip[] collection)
+    {
+        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
+        int index = UnityEngine.Random.Range(0, collection.Length);
+
+        audioSource.clip = collection[index];
+        audioSource.Play();
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        closedLocalPos = transform.localPosition;
+    }
+#endif
+}
+
+public enum ConditionalDoorState
+{
+    Opened,
+    Closed,
+    Busy
 }
