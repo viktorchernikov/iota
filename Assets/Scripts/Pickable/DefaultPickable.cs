360484a17af039588c00b72a8f9d126b222e4a40
Dan <danik.serduk@gmail.com>
2024-11-22 18:19:43 +0100

    feature: add ability to take IPickupble objects, make IPicaupable objects interactive, make that player have no ability to take katana when he is holding something

---
+++ Assets/Scripts/Pickable/DefaultPickable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class DefaultPickable : MonoBehaviour, IPickupable
+{
+    [SerializeField] private float _holdingDistance = 3;
+    
+    public bool IsPicked { get; private set; } = false;
+
+    public Transform self => transform;
+    public float holdingDistance => _holdingDistance;
+    
+    private Rigidbody _rigidbody;
+
+    public void Throw(Vector3 lookDir, float force)
+    {
+        IsPicked = false;
+    }
+
+    public bool CanInteract(IInteractor interactor)
+    {
+        return !IsPicked;
+    }
+
+    public void DropItself()
+    {
+        IsPicked = false;
+    }
+
+    public void OnInteract(IInteractor interactor)
+    {
+        IsPicked = true;
+    }
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+}
+   
--- Assets/Scripts/Player/PlayerKatana.cs
+++ Assets/Scripts/Player/PlayerKatana.cs
@@ -6,14 +6,33 @@ using UnityEngine.Serialization;
 public class PlayerKatana : MonoBehaviour
 {
     [SerializeField] private GameObject _katanaModel;
+
     [SerializeField] private PlayerKatanaState _state = PlayerKatanaState.Absent;
+
     // TODO: maybe better to make separate component for player input
     [SerializeField] private KeyCode _keyToHoldKatana = KeyCode.Alpha1;
-    
+
     public event Action<PlayerKatanaState, PlayerKatanaState> OnKatanaStateChanged;
     public PlayerKatanaState State => _state;
 
     private PlayerInteractionSeeker _seeker;
+    
+    public void SetKatanaUnavailable()
+    {
+        if (_state is PlayerKatanaState.Absent or PlayerKatanaState.Unavailable) return;
+        
+        _katanaModel.SetActive(false);
+        var newState = PlayerKatanaState.Unavailable;
+        OnKatanaStateChanged?.Invoke(_state, newState);
+        _state = newState;
+    }
+
+    public void SetKatanaAvailable()
+    {
+        if (_state is not PlayerKatanaState.Unavailable or PlayerKatanaState.Absent) return;
+
+        HideKatana();
+    }
 
     private void Awake()
     { 
@@ -28,7 +47,8 @@ public class PlayerKatana : MonoBehaviour
     {
         if (!obj.CompareTag("katana") || _state != PlayerKatanaState.Absent) return;
 
-        HoldKatana();
+        if (_state == PlayerKatanaState.Unavailable) HideKatana();
+        else HoldKatana();
     }
 
     private void Update()
@@ -45,6 +65,7 @@ public class PlayerKatana : MonoBehaviour
         { 
             { PlayerKatanaState.Hided, HoldKatana },
             { PlayerKatanaState.Holding, HideKatana},
+            {PlayerKatanaState.Unavailable, () => {}},
             { PlayerKatanaState.Absent, () => throw new NotImplementedException("Katana state not implemented.")}
         };
 
@@ -73,6 +94,7 @@ public enum PlayerKatanaState
     Absent,
     Hided,
     Holding,
+    Unavailable,
 }
 
   
