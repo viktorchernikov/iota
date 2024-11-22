12919b39e1e73025de59fea1fe2d87665966bcbf
Nickname <88354270+Nickn4meDEV@users.noreply.github.com>
2024-11-22 23:02:58 +0100

    Working keypad with rework

--- Assets/Scripts/PuzzleParts/Keypad/KeypadKey.cs
+++ Assets/Scripts/PuzzleParts/Keypad/KeypadKey.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KeypadKey : MonoBehaviour
+public class KeypadKey : MonoBehaviour, IInteractable
 {
     #region State
     public bool IsPressed
@@ -23,33 +23,35 @@ public class KeypadKey : MonoBehaviour
     [SerializeField] KeypadController controller;
     #endregion
 
-    //public InteractableHoverResponse GetHoverResponse(IInteractor interactor)
-    //{
-    //    if (IsPressed)
-    //        return InteractableHoverResponse.None;
+    public InteractableHoverResponse GetHoverResponse(IInteractor interactor)
+    {
+        if (IsPressed)
+            return InteractableHoverResponse.None;
 
-    //    return controller.IsActive ? InteractableHoverResponse.Disable : InteractableHoverResponse.Enable;
-    //}
+        return controller.IsActive ? InteractableHoverResponse.None : InteractableHoverResponse.Enable;
+    }
 
     public bool CanInteract(IInteractor interactor)
     {
         Player player = interactor as Player;
         if (!player) return false;
 
-        return true;
+        return !IsPressed;
     }
 
     public void OnInteract(IInteractor interactor)
     {
         if (IsPressed)
             return;
-        OnPressedKey();
+        StartCoroutine(OnPressedSequence());
     }
 
-    public void OnPressedKey()
+    IEnumerator OnPressedSequence()
     {
         IsPressed = true;
         controller.PasswordEntry(sKey);
+
+        yield return new WaitForSeconds(controller.pressedDelay);
 
         IsPressed = false;
     }
